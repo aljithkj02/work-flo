3f3c12b779ddc135f4a903d261a2c89109c78b39
aljithkj02 <kwdeathlok@gmail.com>
2024-07-28 14:14:42 +0530

    Add User model

---
+++ server/src/db/models/user.model.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcryptjs';
+import { IUser } from '@/utils/types/model.type';
+
+
+const UserSchema: Schema<IUser> = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/\S+@\S+\.\S+/, 'Email is invalid'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+UserSchema.pre('save', async function save(next) {
+  const user = this as IUser;
+  if (!user.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    return next();
+  } catch (err) {
+    return next(err as Error);
+  }
+});
+
+UserSchema.methods.comparePassword = async function (candidatePassword: string) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+export const User = mongoose.model<IUser>('User', UserSchema);
